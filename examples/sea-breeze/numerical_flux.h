335e4bc200eb35bbd321bc2726bf482246b47fe7
Ondrej Certik <ondrej@certik.cz>
2010-01-14 13:18:25 -0800

    Numerical flux matrices added

--- examples/sea-breeze/numerical_flux.h
+++ examples/sea-breeze/numerical_flux.h
@@ -4,12 +4,6 @@
 #include "hermes2d.h"
 #include "params.h"
 
-double matrix_R(int i, int j, double w0, double w1, double w3, double w4);
-double matrix_R_inv(int i, int j, double w0, double w1, double w3, double w4);
-double matrix_D_minus(int i, int j, double w0, double w1, double w3, double w4);
-void T_rot(double result[4][4], double beta);
-void dot_vector(double result[4], double A[4][4], double B[4]);
-void flux_riemann(double result[4], double w_l[4], double w_r[4]);
-void flux_riemann_invert(double result[4], double w_l[4], double w_r[4]);
+void flux();
 
 #endif
